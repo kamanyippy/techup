7dba9991f6df81f2fdddabbc7d2d00ebc62915e1
kamanyippy <kamanyippy@gmail.com>
2025-05-06 08:58:30 +0000

    added age button

--- script.js
+++ script.js
@@ -35,3 +35,14 @@ while (age < 70) {
 function sayHello() {
     alert("Hello everyone");
 }
+
+function showAge() {
+
+    // Get the value from the input field of ID #user-input
+    // Store this value in a variable called user_input
+    var user_input = document.getElementById("user-input").value
+
+    // Update the content for the HTML element with ID #header-age with 
+    // whatever the user_input varible contains
+    document.getElementById("header-age").textContent = user_input
+}
